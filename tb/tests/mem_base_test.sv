010af6b4cca28748f8ca9228638284b6f673bf01
KrzysztofWojtasik <kw300964@student.polsl.pl>
2026-07-09 16:13:18 +0200

    Add UVM timeout drain time and topology print

--- tb/tests/mem_base_test.sv
+++ tb/tests/mem_base_test.sv
@@ -14,10 +14,19 @@ class mem_base_test extends uvm_test;
         m_env = mem_env::type_id::create("m_env", this);
     endfunction
 
+    function void end_of_elaboration_phase(uvm_phase phase);
+        super.end_of_elaboration_phase(phase);
+
+        uvm_top.set_timeout(500ms, 1);
+        uvm_top.print_topology();
+    endfunction
+
     task main_phase(uvm_phase phase);
         mem_basic_seq seq;
 
         phase.raise_objection(this);
+
+        phase.phase_done.set_drain_time(this, 10ms);
 
         `uvm_info(get_full_name(), "Starting mem_basic_seq", UVM_LOW)
 
